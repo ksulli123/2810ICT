b685c26c23ae9cccb7794de2b8c05bfbc84f4f38
Andrei Ionescu <andrei.ionescu@griffithuni.edu.au>
2017-08-04 23:08:14 +1000

    Added shortest path function
    Added shortest path function, Node class and also added asking which function to use (shortest path or quick path)

--- word_ladder.py
+++ word_ladder.py
@@ -1,4 +1,10 @@
 import re
+import queue
+
+class Node:
+  def __init__(self, word, parent):
+    self.parent = parent
+    self.word = word
 
 # Function for returning how many letters item & target have in common
 def same(item, target):
@@ -11,6 +17,36 @@ def build(pattern, words, seen, list):
   return [word for word in words
                  if re.search(pattern, word) and word not in seen.keys() and
                     word not in list]
+
+# Function for getting the guaranteed shortest path
+def shortestFind(word, words, target, seen):
+  length = len(target)
+  wordQueue = queue.Queue()
+  wordQueue.put(Node(word, None),False)
+
+  # Depth First Search using a Queue
+  while(not wordQueue.empty()):
+    node = wordQueue.get(False)
+    list = []
+    # Generate all of the words that can be created by changing one letter in node.word
+    for i in range(length):
+      list += build(node.word[:i] + "." + node.word[i + 1:], words, seen, list)
+
+    # Loop through each word in the list
+    for w in list:
+      seen[w] = True
+      # If the word is one away from the target the path has been found
+      if same(w, target) == length - 1:
+        path = [target, w, node.word]
+        # Backtrack through all of the parents to get the path then return it
+        while node.parent is not None:
+          node = node.parent
+          path.append(node.word)
+        return len(path) - 1, path[::-1]
+
+      # If the word isn't one away from the target place it in the queue
+      wordQueue.put(Node(w, node), False)
+  return False
 
 def find(word, words, seen, target, path):
   list = []
@@ -49,7 +85,7 @@ while True:
 
 # Statement for making sure that valid start and target words have been input
 while True:
-  start = input("Enter start word:")
+  start = input("Enter start word: ").lower()
   words = []
   for line in lines:
     word = line.rstrip()
@@ -58,21 +94,32 @@ while True:
   if start not in words:
     print("Start word not in dictionary, please try another start word.")
     continue
-  target = input("Enter target word:")
+  target = input("Enter target word: ").lower()
   if len(start)!=len(target):
     print("Length of start word and target word is different, please enter 2 words of same length.")
     continue
   if target not in words:
     print("Target word not in dictionary, please try again.")
     continue
+  # Asking which function to use
+  type = input("Want the shortest path? (y,n): ").lower()
+  if type == "y":
+    seen = {start: True}
+    result = shortestFind(start, words, target, seen)
+    if result != False:
+      print("Steps:", result[0])
+      print("Path:", result[1])
+    else:
+      print("No Path Exists")
+  elif type == "n":
+    path = [start]
+    seen = {start: True}
+    if find(start, words, seen, target, path):
+      path.append(target)
+      print(len(path) - 1, path)
+    else:
+      print("No path found")
+  else:
+    print("Incorrect y/n value, please try again.")
+    continue
   break
-
-count = 0
-path = [start]
-seen = {start : True}
-if find(start, words, seen, target, path):
-  path.append(target)
-  print(len(path) - 1, path)
-else:
-  print("No path found")
-
