b7ede3408a69a48f24d53aae49141c9584f6f04d
Andrei Ionescu <andrei.ionescu@griffithuni.edu.au>
2017-08-04 20:21:40 +1000

    Added comments
    Comments were added to make the functions and statements more understandable

--- word_ladder.py
+++ word_ladder.py
@@ -1,9 +1,13 @@
 import re
 
+# Function for returning how many letters item & target have in common
 def same(item, target):
+  # Adds the letter c represents to a list if it is in the target as well, number of letters similar is returned
   return len([c for (c, t) in zip(item, target) if c == t])
 
+# Function for returning a list of words one letter different than a certain word
 def build(pattern, words, seen, list):
+  # Adds 'word' to a list if it is one letter different, not been seen & not in the list already, returns the new list
   return [word for word in words
                  if re.search(pattern, word) and word not in seen.keys() and
                     word not in list]
@@ -28,14 +32,22 @@ def find(word, words, seen, target, path):
       return True
     path.pop()
 
+# Statement for making sure a valid dictionary has been entered
 while True:
   try:
     fname = input("Enter dictionary name: ")
     file = open(fname)
     lines = file.readlines()
+	
+	# Check to see if the dictionary is empty
+    if not lines:
+      print("The dictionary given is empty")
+      continue
     break
   except FileNotFoundError as e:
     print("Dictionary file by that name does not exist. Please try an existing dictionary name.")
+
+# Statement for making sure that valid start and target words have been input
 while True:
   start = input("Enter start word:")
   words = []
